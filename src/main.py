80f39f4693b5607568b5b0406229952b9257db63
AaronBenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2025-07-13 11:41:22 -0400

    Made downloading threaded

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from ebooklib import epub, ITEM_DOCUMENT
 from os import environ, remove
 from pathlib import Path
 from re import search, sub
+from math import ceil
 
 load_dotenv()
 output_directory = Path(environ.get("OUTPUT_DIRECTORY"))
 
 
 def ascii_only(string: str):
-    string=string.replace(' ','_')
+    string = string.replace(" ", "_")
     return sub(
         r"[^qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890\-\_)(!`~.><?\[\]{}]",
         "",
@@ -29,13 +30,19 @@ subs = session.get_subscriptions(use_threading=True)
 works = [sub for sub in subs if isinstance(sub, Work)]
 
 # Load metadata for works (threaded)
-print("Reloading works")
-threads = []
-for work in works:
-    work.set_session(session)
-    threads.append(work.reload(threaded=True, load_chapters=False))
-for thread in threads:
-    thread.join()
+# Batches threads to avoid ratelimits
+n = 10
+for i in range(0, len(works), n):
+    print(f"Reloading batch: {ceil(i/n)+1}/{ceil(len(works)/n)}")
+
+    works_to_reload = works[i : i + n]
+
+    threads = []
+    for work in works_to_reload:
+        work.set_session(session)
+        threads.append(work.reload(threaded=True, load_chapters=False))
+    for thread in threads:
+        thread.join()
 
 
 # Remove works that do not need to be downloaded (word-count and modify-date unchanged)
@@ -59,7 +66,12 @@ for work in works:
         continue
 
     # Open existing .epub file
-    epub_file = epub.read_epub(work_path)
+    try:
+        epub_file = epub.read_epub(work_path)
+    except epub.EpubException:
+        remove(work_path)
+        works_to_download.append(work)
+        continue
 
     # Extract all chapters
     chapters = [chapter for chapter in epub_file.get_items_of_type(ITEM_DOCUMENT)]
@@ -83,22 +95,52 @@ for work in works:
 
 
 # Download works
-for work in works_to_download:
-    print(f"Downloading: {work.title}")
+# for work in works_to_download:
+#     print(f"Downloading: {work.title}")
 
-    fandom = ascii_only(work.fandoms[0]) if work.fandoms else "Other"
-    series = work.series[0] if work.series else None
+#     fandom = ascii_only(work.fandoms[0]) if work.fandoms else "Other"
+#     series = work.series[0] if work.series else None
 
-    work_path = output_directory / fandom
+#     work_path = output_directory / fandom
 
-    if series:
-        work_path = work_path / ascii_only(series.name)
+#     if series:
+#         work_path = work_path / ascii_only(series.name)
 
-    work_path = work_path / (ascii_only(work.title).replace(" ", "_") + ".epub")
+#     work_path = work_path / (ascii_only(work.title).replace(" ", "_") + ".epub")
 
-    # Make parent directories
-    work_path.parent.mkdir(parents=True, exist_ok=True)
+#     # Make parent directories
+#     work_path.parent.mkdir(parents=True, exist_ok=True)
 
-    # Download fic
-    with open(work_path, "wb") as file:
-        file.write(work.download("EPUB"))
+#     # Download fic
+#     with open(work_path, "wb") as file:
+#         file.write(work.download("EPUB"))
+
+# Download works (threaded)
+# Batches threads to avoid ratelimits
+n = 10
+for i in range(0, len(works_to_download), n):
+    print(f"Downloading batch: {ceil(i/n)+1}/{ceil(len(works_to_download)/n)}")
+
+    works = works_to_download[i : i + n]
+    threads = []
+    for work in works:
+
+        print(f"Downloading: {work.title}")
+
+        fandom = ascii_only(work.fandoms[0]) if work.fandoms else "Other"
+        series = work.series[0] if work.series else None
+
+        work_path = output_directory / fandom
+
+        if series:
+            work_path = work_path / ascii_only(series.name)
+
+        work_path = work_path / (ascii_only(work.title).replace(" ", "_") + ".epub")
+
+        # Make parent directories
+        work_path.parent.mkdir(parents=True, exist_ok=True)
+
+        work.set_session(session)
+        threads.append(work.download_to_file(work_path, "EPUB", threaded=True))
+    for thread in threads:
+        thread.join()
