bc77b195b36e15460f4b2b6cca6076ec153c39bf
Aron BenDaniel <144371000+AaronBenDaniel@users.noreply.github.com>
2026-03-15 18:33:40 -0400

    Does not fail if everything succeeds

--- src/main.py
+++ src/main.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     with start_task(action_type="Get Subs"):
         print("Retrieving subscriptions")
         subs = session.get_subscriptions(use_threading=True)
-        if session.exceptions:
+        if getattr(session,"exceptions",None):
             Message.log(num_sub_page_failures=str(len(session.exceptions)))
 
     # Remove all non-works
